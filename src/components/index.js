beaae7c0f0f0c720299703f2fe87c06e170aaf68
Gabriel Henrique <htm.gabriel@gmail.com>
2021-10-13 07:32:12 -0300

    Created file for the exports all components

---
+++ src/components/index.js
@@ -0,0 +1,17 @@
+import MenuMobile from "./MenuMobile";
+import ModalLogin from "./ModalLogin";
+import Input from "./Input";
+import Button from "./Button";
+import CardLessons from "./CardLessons";
+import InputRadio from "./InputRadio";
+import Pagination from "./Pagination";
+
+export {
+	MenuMobile,
+	ModalLogin,
+	Button,
+	Input,
+	CardLessons,
+	InputRadio,
+	Pagination
+}
